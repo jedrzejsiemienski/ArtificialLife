691ad71cf3fa1aed8a17614190d5da01db9b1071
jenczuen <jenczuen@gmail.com>
2014-05-13 19:14:29 +0200

    added dummy brain

---
+++ brains/BrainDummy.cpp
@@ -0,0 +1,26 @@
+/*
+ * BrainDummy.cpp
+ *
+ *  Created on: 13 maj 2014
+ *      Author: jedrek
+ */
+
+#include "BrainDummy.h"
+
+BrainDummy::BrainDummy() {
+	dataCount = 5;
+}
+
+BrainDummy::~BrainDummy() {
+
+}
+
+int ** BrainDummy::stimulate(int b1, int b2, int a1, int a2, int a3){
+	int ** result = new int*[dataCount];
+	for(int i = 0; i < dataCount; i++){
+		result[i] = new int[2];
+		result[i][0] = 1;
+		result[i][1] = 0;
+	}
+	return result;
+}
---
+++ brains/BrainDummy.h
@@ -0,0 +1,22 @@
+/*
+ * BrainDummy.h
+ *
+ *  Created on: 13 maj 2014
+ *      Author: jedrek
+ */
+
+#ifndef BRAINDUMMY_H_
+#define BRAINDUMMY_H_
+
+#include "BaseBrain.h";
+
+class BrainDummy : public BaseBrain {
+public:
+	BrainDummy();
+	virtual ~BrainDummy();
+	int ** stimulate(int b1, int b2, int a1, int a2, int a3);
+private:
+	int dataCount;
+};
+
+#endif /* BRAINDUMMY_H_ */
